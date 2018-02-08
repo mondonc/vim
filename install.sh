1fdda94714a369bb5302d3e5cacd16ef11583c88
Clément Mondon <clement.mondon@inria.fr>
2018-02-08 08:51:03 +0100

    Add Vundle installation

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 DIR_VIM_GIT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Installing ${HOME}/.vimrc"
+echo "Installing Vundle"
+mkdir -p "${HOME}/.vim/bundle"
+[ -d ~/.vim/bundle/Vundle.vim ] || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+echo "Installing ${HOME}/.vimrc"
+[ -e ${HOME}/.vimrc ] && rm ${HOME}/.vimrc
 ln -s "${DIR_VIM_GIT}/vimrc" "${HOME}"/.vimrc 
+
+vim +PluginInstall +qall
